28d24abb034fbbeee30571e353d2c291580f79ec
Swayam003 <swayam.roxx@gmail.com>
2021-09-18 23:44:34 +0530

    Stage 2 complete

--- src/split_data.py
+++ src/split_data.py
@@ -1,10 +1,12 @@
 # Preprocess the data
+# Scalling the preprocessed data
 # split the preprocessed data
 # save it in data/processed folder
 import os
 import argparse
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from get_data import read_params
 
 def preprocess(config_path):
@@ -12,7 +14,7 @@ def preprocess(config_path):
     raw_data_path = config["load_data"]["raw_dataset_csv"]
     data = pd.read_csv(raw_data_path, sep=",")
 
-    # replacing zero values with the mean of the column
+    # replacing zero values with the mean of the columns
     data['BMI'] = data['BMI'].replace(0, data['BMI'].mean())
     data['BloodPressure'] = data['BloodPressure'].replace(0, data['BloodPressure'].mean())
     data['Glucose'] = data['Glucose'].replace(0, data['Glucose'].mean())
@@ -38,26 +40,39 @@ def preprocess(config_path):
     q = data_cleaned['Age'].quantile(0.99)
     # we are removing the top 1% data from the Age column
     data_cleaned = data_cleaned[data_cleaned['Age'] < q]
-    print(data_cleaned.head())
     return config, data_cleaned
 
+def scalling_data(confiq_path):
+    config, data = preprocess(confiq_path)
+    x = data.drop(columns=['Outcome'])
+    scalar = StandardScaler()
+    X_scaled = scalar.fit_transform(x)
+    Xscaled = pd.DataFrame(X_scaled, columns=['Pregnancies','Glucose','BloodPressure','SkinThickness','Insulin','BMI','DiabetesPedigreeFunction','Age'])
+    return config,data,Xscaled
+
 def split_and_saved_data(config_path):
-    config, df = preprocess(config_path)
+    config, df,Xscaled = scalling_data(config_path)
+    y = df['Outcome']
     #config = read_params(config_path)
-    test_data_path = config["split_data"]["test_path"]
-    train_data_path = config["split_data"]["train_path"]
+    x_test_data_path = config["split_data"]["x_test_path"]
+    y_test_data_path = config["split_data"]["y_test_path"]
+    x_train_data_path = config["split_data"]["x_train_path"]
+    y_train_data_path = config["split_data"]["y_train_path"]
     #raw_data_path = config["load_data"]["raw_dataset_csv"]
     split_ratio = config["split_data"]["test_size"]
     random_state = config["base"]["random_state"]
 
     #df = pd.read_csv(raw_data_path, sep=",")
-    train, test = train_test_split(
-        df,
+    x_train, x_test, y_train, y_test = train_test_split(
+        Xscaled,y,
         test_size=split_ratio,
         random_state=random_state
         )
-    train.to_csv(train_data_path, sep=",", index=False, encoding="utf-8")
-    test.to_csv(test_data_path, sep=",", index=False, encoding="utf-8")
+    x_train.to_csv(x_train_data_path, sep=",", index=False, encoding="utf-8")
+    y_train.to_csv(y_train_data_path, sep=",", index=False, encoding="utf-8")
+    x_test.to_csv(x_test_data_path, sep=",", index=False, encoding="utf-8")
+    y_test.to_csv(y_test_data_path, sep=",", index=False, encoding="utf-8")
+
 
 if __name__=="__main__":
     args = argparse.ArgumentParser()
